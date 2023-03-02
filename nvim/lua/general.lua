69b9ccff078b9172baa34085446fa6bc7bb8c9a1
korei999 <93387739+korei999@users.noreply.github.com>
2023-03-02 23:20:20 +0200

    Update general.lua

--- nvim/lua/general.lua
+++ nvim/lua/general.lua
@@ -25,21 +25,21 @@ vim.opt['undoreload'] = 10000
 
 -- Stop comments on newline / ctrl-d for "delete"
 vim.cmd([[
-    autocmd BufWinEnter * :set formatoptions-=c formatoptions-=r formatoptions-=o
-    autocmd BufWinEnter * :set nowrap
-    imap <C-D> <C-O>x
-    nnoremap d "_d
-    nnoremap D "_D
-    vnoremap d "_d
+autocmd BufWinEnter * :set formatoptions-=c formatoptions-=r formatoptions-=o
+autocmd BufWinEnter * :set nowrap
 ]])
 
--- autocmd BufWinEnter * :set formatoptions-=c formatoptions-=r formatoptions-=o
 -- highlight Normal guibg=none
 -- nnoremap x "_x
 
+vim.keymap.set("n", "d", "\"_d")
+vim.keymap.set("n", "D", "\"_D")
+vim.keymap.set("v", "d", "\"_d")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("i", "<C-D>", "<C-O>x")
