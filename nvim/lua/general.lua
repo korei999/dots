caf63ea665b9c4428df329443d22f3f201f97b37
korei999 <93387739+korei999@users.noreply.github.com>
2023-03-02 23:28:49 +0200

    Update general.lua

--- nvim/lua/general.lua
+++ nvim/lua/general.lua
@@ -1,36 +1,38 @@
 require('monokai').setup { 
-	palette = require('monokai').dark,
-	italics = false,
+	palette = require('monokai'),
+	italics = true,
 }
-vim.opt['number'] = true
-vim.opt['relativenumber'] = true
-vim.opt['smarttab'] = true
-vim.opt['expandtab'] = false
-vim.opt['tabstop'] = 4
-vim.opt['shiftwidth'] = 4
-vim.opt['hlsearch'] = false
-vim.opt['hidden'] = true
-vim.opt['clipboard'] = 'unnamedplus'
-vim.opt['mouse'] = 'a'
-vim.opt['swapfile'] = false
-vim.opt['smartcase'] = true
-vim.opt['guicursor'] = 'i:block'
-vim.opt['cmdheight'] = 1
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.smarttab = true
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.hlsearch = false
+vim.opt.hidden = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.mouse = 'a'
+vim.opt.swapfile = false
+vim.opt.smartcase = true
+vim.opt.guicursor = 'i:block'
+vim.opt.cmdheight = 1
+vim.opt.scrolloff = 8;
 
 -- Persistent undo
-vim.opt['undofile'] = true
+vim.opt.undofile = true
 vim.opt.undodir = os.getenv( "HOME" ) .. '/.cache/nvim/undo'
-vim.opt['undolevels'] = 1000
-vim.opt['undoreload'] = 10000
+vim.opt.undolevels = 1000
+vim.opt.undoreload = 10000
 
--- Stop comments on newline / ctrl-d for "delete"
+-- Stop comments on newline
 vim.cmd([[
 autocmd BufWinEnter * :set formatoptions-=c formatoptions-=r formatoptions-=o
 autocmd BufWinEnter * :set nowrap
 ]])
 
+-- nnoremap d "_d
+-- autocmd BufWinEnter * :set formatoptions-=c formatoptions-=r formatoptions-=o
 -- highlight Normal guibg=none
--- nnoremap x "_x
 
 vim.keymap.set("n", "d", "\"_d")
 vim.keymap.set("n", "D", "\"_D")
