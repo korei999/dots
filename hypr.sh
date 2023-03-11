041108f46ffaa7e7a7db3daf119205b6dda899b8
korei999 <93387739+korei999@users.noreply.github.com>
2023-03-11 14:19:16 +0200

    Add files via upload

---
+++ hypr.sh
@@ -0,0 +1,24 @@
+export XDG_CURRENT_DESKTOP=Hyprland
+export XDG_SESSION_TYPE=wayland
+export XDG_SESSION_DESKTOP=Hyprland
+
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_QPA_PLATFORM="wayland;xcb"
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+export QT_QPA_PLATFORMTHEME=qt5ct
+export CLUTTER_BACKEND="wayland"
+
+export GTK_THEME="Material-Black-Blueberry-3.36"
+export XCURSOR_THEME="Adwaita"
+# export XCURSOR_SIZE="28"
+
+export MOZ_ENABLE_WAYLAND=1
+export MOZ_WEBRENDER=1
+export _JAVA_AWT_WM_NONREPARENTING=1
+export SDL_VIDEODRIVER=wayland
+export PIPEWIRE_LATENCY="128/48000"
+
+# export LOCK_BG="/home/korei/Pictures/wallpapers/SetupLenovo.png"
+# export LOCK="swaylock -f -c 121212 -i $LOCK_BG -s fill --font Spleen --font-size 28 --indicator-radius 90 --indicator-thickness 15"
+
+Hyprland
